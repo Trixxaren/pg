ca005f22ca4617b07dda43dd5b6fa928a2effcc2
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-10-28 14:28:17 +0100

    Counter added

---
+++ src/components/Counter/Counter.stories.jsx
@@ -0,0 +1,15 @@
+import Counter from "./Counter.jsx";
+
+export default {
+  title: "Components/Counter",
+  component: Counter,
+  tags: ["autodocs"],
+};
+
+export const Default = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};

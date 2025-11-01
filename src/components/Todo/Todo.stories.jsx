993bd57b024ae645528bb35592e04447174efd00
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-11-01 16:14:49 +0100

    todo started

---
+++ src/components/Todo/Todo.stories.jsx
@@ -0,0 +1,14 @@
+import Todo from "./Todo";
+
+export default {
+  title: "Components/Todo",
+  component: Todo,
+};
+
+export const Default = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+};

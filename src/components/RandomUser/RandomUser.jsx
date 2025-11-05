163c0c2dfb623d01e3f5f4e4b5f5eae02cafdb46
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-11-05 16:27:05 +0100

    RandomUser added loading

--- src/components/RandomUser/RandomUser.jsx
+++ src/components/RandomUser/RandomUser.jsx
@@ -3,25 +3,35 @@ import styles from "./RandomUser.module.css";
 
 const RandomUser = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false); // nytt state för laddning
 
   const fetchUser = () => {
+    setLoading(true); // visar att vi börjar ladda
     fetch("https://randomuser.me/api")
       .then((res) => res.json())
       .then((data) => {
         setUser(data.results[0]);
+        setLoading(false); // färdigladdad
         console.log("hämtad användare:", data.results[0]);
       })
-      .catch((err) => console.error("Fel vid hämtning:", err));
+      .catch((err) => {
+        console.error("Fel vid hämtning:", err);
+      })
+      .finally(() => {
+        setLoading(false); // körs alltid – oavsett success/fel
+      });
   };
 
   return (
     <div className={styles.wrapper}>
-      <h2>Slumäpmässig användare</h2>
-      <button onClick={fetchUser} className={styles.button}>
-        Hämta användare
+      <h2>Slumpmässig användare</h2>
+      <button onClick={fetchUser} className={styles.button} disabled={loading}>
+        {loading ? "Hämtar..." : "Hämta användare"}
       </button>
 
-      {user && (
+      {loading && <p className={styles.loading}>⏳ Laddar data...</p>}
+
+      {user && !loading && (
         <div className={styles.card}>
           <img
             src={user.picture.large}
@@ -34,7 +44,7 @@ const RandomUser = () => {
             {user.name.first} {user.name.last}
           </h3>
           <p>
-            <span style={{ color: "green" }}>Age:</span>
+            <span style={{ color: "green" }}>Age: </span>
             {user.dob.age}
           </p>
           <h4>
